8ffc2885705ad9b736bd75033e818c3f76ff9d12
Auravision <auravision.africa@gmail.com>
2024-02-22 12:23:21 +0300

    moving on to Implementing Wander Algorithm and EnemyAI

--- Assets/_Aura/Scripts/Monobehaviours/Gameplay/Character.cs
+++ Assets/_Aura/Scripts/Monobehaviours/Gameplay/Character.cs
@@ -12,7 +12,7 @@ public abstract class Character : MonoBehaviour
     /// 
     /// </summary>
     /// <param name="damage">amount of damage</param>
-    /// <param name="interval">time interval in case of recurring damge application</param>
+    /// <param name="interval">time interval in case of recurring damge application e.g walking on lava</param>
     /// <returns></returns>
     public abstract IEnumerator DamageCharacter(int damage, float interval);
 
